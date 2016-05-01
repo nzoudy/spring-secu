97250d2e777a7fe4a836187cf419f0b381ce53f0
nzouda urbain <nzoudy@hotmail.fr>
2016-05-01 21:12:37 +0200

    -a

--- src/main/java/com/zoudys/service/EtudiantRestService.java
+++ src/main/java/com/zoudys/service/EtudiantRestService.java
@@ -1,8 +1,19 @@
 package com.zoudys.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +27,34 @@ public class EtudiantRestService {
 	@Autowired
 	private EtudiantRepository etudiantRepository;
 	
+	@Secured(value={"ROLE_ADMIN","ROLE_SCOLARITE"})
 	@RequestMapping(value="/saveEtudiant", method=RequestMethod.GET)
 	public Etudiant saveEtudiant(Etudiant e){
 		
 		return etudiantRepository.save(e);
 	}
 	
+	@Secured(value={"ROLE_ADMIN","ROLE_SCOLARITE","ROLE_PROF","ROLE_ETUDIANT"})
 	@RequestMapping(value="/etudiants", method=RequestMethod.GET)
 	public Page<Etudiant> listEtudiants(int page, int size){
 		
 		return etudiantRepository.findAll(new PageRequest(page, size));
 	}
+	
+	@RequestMapping(value="/getLogedUser")
+	public Map<String, Object> getLogedUser(HttpServletRequest httpServletRequest){
+		HttpSession httpSession = httpServletRequest.getSession();
+		SecurityContext securityContext = (SecurityContext) httpSession.getAttribute("SPRING_SECURITY_CONTEXT");
+		String username=securityContext.getAuthentication().getName();
+		List<String> roles=new ArrayList<>();
+		for(GrantedAuthority ga : securityContext.getAuthentication().getAuthorities()){
+			roles.add(ga.getAuthority());
+		}
+		Map<String, Object> params=new HashMap<>();
+		params.put("username", username);
+		params.put("roles", roles);
+		return params;
+	}
+	
 	
 }
